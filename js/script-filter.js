6a28b2dbf22750700e9764b8747b2e324467c35d
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2023-06-28 18:00:26 -0300

    Exemplo com o FILTER

--- js/script-filter.js
+++ js/script-filter.js
@@ -1,3 +1,19 @@
+// Retorna um novo array somente com os números > 37.
 const randomNumbers = [36, 99, 37, 63]
 
-const numbersGreaterThan37 = randomNumbers.filter()
+const numbersGreaterThan37 = randomNumbers.filter(item => item > 37)
+
+console.log(numbersGreaterThan37);
+
+//Retorna um novo array somente com os usuário premium: true
+const users = [
+  { name: 'Ada Lovelace', premium: true },
+  { name: 'Grace Hopper', premium: false },
+  { name: 'Alan Turing', premium: true },
+  { name: 'Linus Torvalds', premium: false },
+  { name: 'Margaret Hamilton', premium: true }
+]
+
+const premiumUsers = users.filter(user => user.premium)
+
+console.log(premiumUsers);
